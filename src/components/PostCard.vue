34a94b55aecb3282b4f127e4759fc869f294f416
john lundgren <john.lundgren.lexicon@gmail.com>
2021-05-20 10:14:10 +0200

    Rendering Post previews.

---
+++ src/components/PostCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <!-- Card Wider -->
+  <div class="card card-cascade wider shadow-lg mb-4">
+    <!-- Card image -->
+    <div class="view view-cascade overlay">
+      <img class="card-img-top" :src="post.imgUrl" alt="Card image cap" />
+      <a href="#!">
+        <div class="mask rgba-white-slight"></div>
+      </a>
+    </div>
+
+    <!-- Card content -->
+    <div class="card-body card-body-cascade text-center">
+      <!-- Title -->
+      <h4 class="card-title">
+        <strong>{{ post.title }}</strong>
+      </h4>
+      <!-- Text -->
+      <p class="card-text">{{ post.body | shorten }}...</p>
+    </div>
+  </div>
+  <!-- Card Wider -->
+</template>
+
+<script>
+export default {
+  name: "PostCard",
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+  filters: {
+    shorten: (value) => {
+      return value.substring(0, 400);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="home">
     <h1>Previews</h1>
+    <div class="mt-5">
+      <PostCard v-for="post in getPosts" :key="post.id" :post="post" />
+    </div>
   </div>
 </template>
+<script>
+import { mapGetters } from "vuex";
+import PostCard from "../components/PostCard";
+export default {
+  name: "Home",
+  components: {
+    PostCard,
+  },
+  computed: {
+    ...mapGetters(["getPosts"]),
+  },
+};
+</script>
