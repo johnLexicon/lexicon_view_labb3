5fbb5d56cb74d57145773fa3c4c7d4057470ef87
john lundgren <john.lundgren.lexicon@gmail.com>
2021-05-21 08:49:23 +0200

    Create post logic implemented.

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -1,12 +1,72 @@
 <template>
   <div class="createPost">
-    <h1>Create Post</h1>
+    <h1>Add New Post</h1>
+    <!-- Default form contact -->
+    <form class="text-center border border-light p-5" action="#!">
+      <p class="h4 mb-4">Blog Post</p>
+
+      <!-- Title -->
+      <input
+        type="text"
+        id="title"
+        class="form-control mb-4 rounded-2"
+        placeholder="Title..."
+        v-model="post.title"
+      />
+
+      <!-- Image Url -->
+      <input
+        type="text"
+        id="imageUrl"
+        class="form-control mb-4 rounded-2"
+        placeholder="Image Url"
+        v-model="post.imgUrl"
+      />
+
+      <!-- Content -->
+      <div class="form-group">
+        <textarea
+          class="form-control rounded-2 mb-4"
+          id="content"
+          rows="3"
+          placeholder="Blog Content..."
+          v-model="post.body"
+        ></textarea>
+      </div>
+
+      <!-- Send button -->
+      <button class="btn btn-danger btn-block" type="submit">Send</button>
+    </form>
+    <!-- Default form contact -->
+
+    <h2>Preview Blog</h2>
+    <PostCard v-if="showPreview" :post="post" :showAllText="true" />
   </div>
 </template>
 
 <script>
+import PostCard from "../components/PostCard";
 export default {
   name: "CreatePost",
+  components: {
+    PostCard,
+  },
+  data() {
+    return {
+      post: {
+        id: null,
+        title: "",
+        body: "",
+        likes: 0,
+        imgUrl: null,
+      },
+    };
+  },
+  computed: {
+    showPreview() {
+      return this.post.title || this.post.imgUrl || this.post.body;
+    },
+  },
 };
 </script>
 
